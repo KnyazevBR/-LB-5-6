4207dc685c6b45e19007d3618fec47cbf9038e3a
KnyazevBR <145508470+KnyazevBR@users.noreply.github.com>
2023-11-29 18:54:35 +0300

    Update LR 5-6 1.4.pas

---
+++ LR 5-6 1.4.pas
@@ -0,0 +1,23 @@
+﻿{4.	Заполнить массив А из 30 элементов случайными числами из диапазона
+[-99, 67]. Сформировать массив В, в который записать только четные элементы массива А.}
+
+var a,b:array[1..30] of integer;
+k,c,i: integer;
+begin
+  c:=1;
+  k:= 1;
+  for i:= 1 to 30 do
+    begin
+    a[i]:= Random(-99,67);
+    if (a[i] mod 2 = 0) and (a[i]<>0) then
+     begin
+       b[c]:=a[i];
+       c:=c+1;
+     end;
+    end;
+  for i:= 1 to 30 do
+    write(a[i], ' ');
+  writeln('');
+  for k:= 1 to c-1 do
+    write(b[k], ' ');
+end.

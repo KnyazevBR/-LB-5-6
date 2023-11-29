608cd4bc13c313a75bf87b85d4829c8c57f0ec68
KnyazevBR <145508470+KnyazevBR@users.noreply.github.com>
2023-11-29 18:53:55 +0300

    Update LR 5-6 1.3.pas

---
+++ LR 5-6 1.3.pas
@@ -0,0 +1,41 @@
+﻿var a:array[1..20] of integer;
+i: integer;
+s: integer;
+x: integer;
+y:integer;
+x1:integer;
+begin 
+  s := 20;
+  x1:=-52;
+  x :=65;
+  y:=0;
+  
+  for i:=1 to s do 
+    a[i] := Random(-5,5);
+  for i:=1 to s do
+    write(a[i],' ');
+  
+  for i:= s downto 1 do
+    if (a[i] > x1) and (a[i]>0) then begin {ВЫВОДИТ ИНДЕКС ПОСЛЕДНЕГО НАИБОЛЬШЕГО}
+      x1:= a[i];
+      y:=i;
+      end;
+
+writeln('итог',x1,' ',y); 
+ 
+  for i:= 1 to s do
+    if (a[i] < x) and (a[i]>0) then begin
+      x:= a[i];
+      y:=i;
+      end;
+
+writeln('итог',x,' ',y); 
+      
+  for i:= 1 to s do
+    if a[i] mod 5 = 0 then begin
+      x:= a[i];
+      y:=i;
+      end;
+        
+writeln('итог',x,' ',y);    
+end.
